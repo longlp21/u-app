7902e58def6dfa4ed733c3bbc69571c28b6db7dd
minhtq13 <taminh596@gmail.com>
2022-07-11 11:28:26 +0700

    update chat message

--- src/layouts/messages/client/Chatting.js
+++ src/layouts/messages/client/Chatting.js
@@ -1,13 +1,62 @@
-import React from "react";
 import "./Chatting.css";
+import io from "socket.io-client";
+import { useEffect, useState } from "react";
+// import "../server/server";
 
 function Chatting() {
+  const [message, setMessage] = useState("");
+
+  const form = document.getElementById("form");
+  const input = document.getElementById("input");
+
+  // form.addEventListener("submit", function (e) {
+  //   e.preventDefault();
+  //   if (input.value) {
+  //     socket.emit("chat-message", {
+  //       fromId: userId,
+  //       fromName: userName,
+  //       roomId: roomId,
+  //       roomName: roomName,
+  //       message: input.value,
+  //       status: 1,
+  //     });
+  //   }
+  //   input.value = "";
+  // });
+
+  function addMessage(fromUser, msg) {
+    const messages = document.getElementById("messages");
+    const item = document.createElement("li");
+    item.textContent = msg;
+    if (!fromUser) {
+      item.style.textAlign = "right";
+    } else {
+      item.style.textAlign = "left";
+    }
+    messages.appendChild(item);
+    window.scrollTo(0, document.body.scrollHeight);
+  }
+
+  // useEffect(() => {
+  //   socket.on("receive_message", (msg) => {
+  //     console.log(msg, userId);
+  //     if (msg && msg.fromId != userId) {
+  //       addMessage(msg.fromId, msg.message);
+  //     } else {
+  //       addMessage(null, msg.message);
+  //     }
+  //   });
+  // }, [socket]);
+
   return (
     <div>
-      <ul className="messages1"></ul>
+      <div className="boardChat">
+        <h3 className="title1">Room name</h3>
+        <ul className="messages1"> </ul>
+      </div>
       <form className="form1" action="">
         <input className="input1" autoComplete="off" />
-        <button>Send</button>
+        <button type="submit">Send</button>
       </form>
     </div>
   );
--- src/layouts/messages/server/server.js
+++ src/layouts/messages/server/server.js
@@ -1,4 +1,15 @@
 import "./room-config";
+import { io } from "./socket.io.esm.min";
+
+const express = require("express");
+
+const app = express();
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+
+const server = http.createServer(app);
+
 /*Init socket*/
 const socket = io("http://localhost:5000", {
   reconnectionDelay: 1000,
@@ -10,8 +21,8 @@ const socket = io("http://localhost:5000", {
   rejectUnauthorized: false,
 });
 
-const userName = "user1";
-const userId = 1;
+// const userName = "user1";
+// const userId = 1;
 
 socket.on("connect", () => {
   console.log(socket.id, socket.connected);
@@ -32,15 +43,15 @@ socket.on("reconnect_error", (err) => {
 /*
 Listing join room
 */
-socket.on("join-room", function (room) {
-  console.log(room);
-});
+// socket.on("join-room", function (room) {
+//   console.log(room);
+// });
 
 /*
 Receive message from server
 */
-socket.on("chat-message", function (msg) {
-  /* {
+// socket.on("chat-message", function (msg) {
+/* {
   "_id": "629eaaa8feb02737cc58ca90",
   "fromId": "1",
   "fromName": "user1",
@@ -51,47 +62,51 @@ socket.on("chat-message", function (msg) {
   "status": true,
   "__v": 0
 }*/
-  console.log(msg, userId);
-  if (msg && msg.fromId != userId) {
-    addMessage(msg.fromId, msg.message);
-  } else {
-    addMessage(null, msg.message);
-  }
-});
+//   console.log(msg, userId);
+//   if (msg && msg.fromId != userId) {
+//     addMessage(msg.fromId, msg.message);
+//   } else {
+//     addMessage(null, msg.message);
+//   }
+// });
 
-$(document).ready(function () {
-  // Join room
-  socket.emit("join-room", { userId: userId, username: userName, roomId: roomId });
+// $(document).ready(function () {
+//   // Join room
+//   socket.emit("join-room", { userId: userId, username: userName, roomId: roomId });
 
-  var form = document.getElementById("form");
-  var input = document.getElementById("input");
+//   var form = document.getElementById("form");
+//   var input = document.getElementById("input");
 
-  form.addEventListener("submit", function (e) {
-    e.preventDefault();
-    if (input.value) {
-      socket.emit("chat-message", {
-        fromId: userId,
-        fromName: userName,
-        roomId: roomId,
-        roomName: roomName,
-        message: input.value,
-        status: 1,
-      });
-    }
-    input.value = "";
+//   form.addEventListener("submit", function (e) {
+//     e.preventDefault();
+//     if (input.value) {
+//       socket.emit("chat-message", {
+//         fromId: userId,
+//         fromName: userName,
+//         roomId: roomId,
+//         roomName: roomName,
+//         message: input.value,
+//         status: 1,
+//       });
+//     }
+//     input.value = "";
+//   });
+// });
+
+//  "https://cdn.socket.io/4.3.2/socket.io.esm.min.js";
+io.on("connection", (socket1) => {
+  // console.log(`User Connected: ${data.id}`);
+
+  // socket.on("join-room", function (room) {
+  //   console.log(room);
+  // });
+  // socket.emit("join-room", { userId: userId, username: userName, roomId: roomId });
+
+  socket.on("send_message", (data) => {
+    socket.to(data.room).emit("receive_message", data);
   });
 });
 
-import { io } from "./socket.io.esm.min.js"; //  "https://cdn.socket.io/4.3.2/socket.io.esm.min.js";
-function addMessage(fromUser, msg) {
-  var messages = document.getElementById("messages");
-  var item = document.createElement("li");
-  item.textContent = msg;
-  if (!fromUser) {
-    item.style.textAlign = "right";
-  } else {
-    item.style.textAlign = "left";
-  }
-  messages.appendChild(item);
-  window.scrollTo(0, document.body.scrollHeight);
-}
+server.listen(5000, () => {
+  console.log("SERVER IS RUNNING");
+});
